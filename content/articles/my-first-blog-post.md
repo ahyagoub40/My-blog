9568d2194e0f45a5f2c3bec286226f39a2d32b44
Ahmed Yagoub <ahyagoub40@gmail.com>
2021-05-23 15:13:49 -0600

    Add html an vue comp to markdonw

--- content/articles/my-first-blog-post.md
+++ content/articles/my-first-blog-post.md
@@ -8,3 +8,28 @@ alt: my first blog post
 # My first blog post
 
 Welcome to my first blog post using content module
+
+## This is a heading
+
+This is some more info
+
+### This is a sub heading
+
+This is some more info
+
+### This is another sub heading
+
+This is some more info
+
+## This is another heading
+
+This is some more info
+
+<div class="note">
+  This is HTML inside markdown that has a class of note
+</div>
+<info-box>
+  <template #info-box>
+    This is a vue component inside markdown using slots
+  </template>
+</info-box>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -17,6 +17,15 @@ export default {
 
 <template>
   <article>
+    <nav>
+      <ul>
+        <li v-for="link of article.toc" :key="link.id">
+          <NuxtLink :to="`#${link.id}`" :class="{ 'h2': link.depth === 2, 'h3': link.depth === 3 }">
+            {{ link.text }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
     <h1>{{ article.title }}</h1>
     <p>{{ article.description }}</p>
     <img :src="require(`~/assets/images/${article.img}`)" :alt="article.alt">
@@ -37,5 +46,24 @@ export default {
   }
   .nuxt-content p {
     margin-bottom: 20px;
+    color: rebeccapurple;
   }
+  .icon.icon-link {
+  background-image: url('~assets/images/preview.png');
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-size: 20px 20px;
+}
+.h2 {
+  padding: 2rem 0;
+}
+.h3 {
+  padding-bottom: 2rem;
+  margin-left: 2rem;
+}
+.note {
+  background-color: blue;
+  color: white;
+}
 </style>
